1d3e929eaa9a91f89b8e09e1b16c4e9bb5999cef
arman <arman.seisenbek@gmail.com>
2022-05-24 13:12:02 +0600

    new core-common module

---
+++ core-common/src/main/java/com/onepercent/core/domain/UIComponentState.kt
@@ -0,0 +1,11 @@
+package com.onepercent.core.domain
+
+/**
+ * A generic class for hiding/showing some ui component.
+ */
+sealed class UIComponentState {
+
+    object Show: UIComponentState()
+
+    object Hide: UIComponentState()
+}
